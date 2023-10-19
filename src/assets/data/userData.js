d9e949669116a8ec1b5a562c6adc63e611e3add9
Nauval Fahreza A <nauvalfahreza03@gmail.com>
2023-10-19 10:00:28 +0700

    [ADD] change status

--- src/assets/data/userData.js
+++ src/assets/data/userData.js
@@ -3,96 +3,60 @@ export const userData = [
         name: 'Charlie Brown',
         university: 'Universitas C',
         role: 'Frontend Mobile Developer',
-        status: 'Ready to take Competition'
+        status: 'Ready in competition'
     },
     {
         name: 'David Davis',
         university: 'Universitas D',
         role: 'Backend Developer',
-        status: 'Ready to take Competition'
+        status: 'Ready in competition'
     },
     {
         name: 'Emily Evans',
         university: 'Universitas E',
         role: 'UI Designer',
-        status: 'Ready to take Competition'
+        status: 'Ready in competition'
     },
     {
         name: 'Frankie Franklin',
         university: 'Universitas F',
         role: 'Full Stack Developer',
-        status: 'Ready to take Competition'
+        status: 'Ready in competition'
     },
     {
         name: 'Grace Green',
         university: 'Universitas G',
         role: 'Product Manager',
-        status: 'Ready to take Competition'
+        status: 'Ready in competition'
     },
     {
         name: 'Harry Harris',
         university: 'Universitas H',
         role: 'Mobile App Frontend Developer',
-        status: 'Ready to take Competition'
+        status: 'Ready in competition'
     },
     {
         name: 'Isabel Jackson',
         university: 'Universitas I',
         role: 'UI/UX Designer',
-        status: 'Ready to take Competition'
+        status: 'Ready in competition'
     },
     {
         name: 'Jack Johnson',
         university: 'Universitas J',
         role: 'Game Developer',
-        status: 'Ready to take Competition'
+        status: 'Ready in competition'
     },
     {
         name: 'Mineze',
         university: 'Universitas Brawijaya',
         role: 'Frontend Web Developer',
-        status: 'Ready to take Competition'
+        status: 'Ready in competition'
     },
     {
         name: 'Liam Lee',
         university: 'Universitas L',
         role: 'Data Scientist',
-        status: 'Already to take Competition'
+        status: 'Already in competition'
     },
-    {
-        name: 'Mia Mitchell',
-        university: 'Universitas M',
-        role: 'Data Analyst',
-        status: 'Already to take Competition'
-    },
-    {
-        name: 'Nathan Nelson',
-        university: 'Universitas N',
-        role: 'UI Designer',
-        status: 'Already to take Competition'
-    },
-    {
-        name: 'Olivia Owens',
-        university: 'Universitas O',
-        role: 'Product Manager',
-        status: 'Already to take Competition'
-    },
-    {
-        name: 'Peter Parker',
-        university: 'Universitas P',
-        role: 'Backend Developer',
-        status: 'Already to take Competition'
-    },
-    {
-        name: 'Quincy Quinn',
-        university: 'Universitas Q',
-        role: 'Product Manager',
-        status: 'Already to take Competition'
-    },
-    {
-        name: 'Rachel Reed',
-        university: 'Universitas R',
-        role: 'Cyber Security Specialist',
-        status: 'Already to take Competition'
-    }
 ];
