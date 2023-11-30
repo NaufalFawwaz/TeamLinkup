e895bd742a973c849c4b4285c3d6c5fd847a1cbd
Nauval Fahreza A <nauvalfahreza03@gmail.com>
2023-11-30 14:41:15 +0700

    change logic

--- src/Routes/PrivateRoutes.jsx
+++ src/Routes/PrivateRoutes.jsx
@@ -3,7 +3,7 @@ import { Outlet, Navigate } from "react-router-dom"
 const PrivateRoutes = () => {
     return (
         <>
-            {localStorage.getItem('login') ?
+            {localStorage.getItem('uid') ?
                 <Navigate to={'/'} />
                 :
                 <Outlet />}
